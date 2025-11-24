8ef3c6a7af02425de741b569719ebb1085682c4a
paulovgl <ditoprimo@gmail.com>
2025-11-24 11:32:03 -0300

    Clear Project

--- src/test/java/com/github/paulovgl/sistema_oanse_backend/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package com.github.paulovgl.sistema_oanse_backend;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
